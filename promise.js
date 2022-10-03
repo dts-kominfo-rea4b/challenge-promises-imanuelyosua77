71ff3df20a2f24ed21ffe22a39acaa5e3246161c
imanuelyosua77 <113983223+imanuelyosua77@users.noreply.github.com>
2022-10-03 21:42:18 +0800

    Edit Promise.js add Promise

--- promise.js
+++ promise.js
@@ -1,7 +1,22 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  const XXI = await promiseTheaterIXX();
+  const CGV = await promiseTheaterVGC();
+  const filterXXI = XXI.filter(XXIrun)
+  const filterCGV = CGV.filter(CGVrun);
+  function XXIrun(x) { if (x.hasil == emosi) return x }
+  function CGVrun(y) { if (y.hasil == emosi) return y; }
+
+  return new Promise((resolve, reject) => {
+    if (filterXXI === null && filterCGV === "") {
+      reject('ERROR WOI')
+    } else {
+      resolve(filterXXI.length + filterCGV.length);
+    }
+  })
+}
 
 module.exports = {
   promiseOutput,
